35f50931e69c0bf15b8e3ba66c1a344963aea912
Mikael69-codeSlayer <mikael.sardor@studerande.plushogskolan.se>
2021-01-06 09:33:16 +0100

    Navbar with router-link. Jonathan & Mikael.

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -2,7 +2,17 @@
   <!-- Title -->
   
 <template>
-<h1>{{ filmvisarna }}</h1>
+  <nav>
+    <router-link to="/">Filmvisarna</router-link>
+
+    <a class="tickets" @click="goToTickets">Biljetter</a>
+
+    <a class="filmsAndTrailers" @click="goToFilmsAndTrailer"
+      >Filmer & Trailer</a>
+
+       <a class="login" @click="login">Logga in</a>
+
+  </nav>
 </template>
 
 <script>
@@ -14,5 +24,51 @@ export default {
 };
 </script>
 
-<style>
+
+<style scoped>
+nav {
+  user-select: none;
+  padding: 25px 0 45px 0;
+  text-align: center;
+  background-color: rgb(218, 31, 31);
+  color: rgb(241, 241, 241);
+}
+
+router-link {
+  text-decoration: none;
+  color: rgb(241, 241, 241);
+  font-weight: bold;
+  float: left;
+  padding-left: 10px;
+}
+a {
+  text-decoration: none;
+  color: rgb(241, 241, 241);
+  font-weight: bold;
+  float: left;
+  cursor: pointer;
+}
+
+a:hover {
+  color: rgb(128, 195, 226);
+}
+
+.tickets {
+  padding-left: 20vh;
+}
+
+.filmsAndTrailers {
+  padding-left: 20vh;
+}
+
+.login {
+  float: right;
+  padding-right: 50px;
+}
+
+.login:hover {
+  color: rgb(128, 195, 226);
+
+}
+
 </style>
