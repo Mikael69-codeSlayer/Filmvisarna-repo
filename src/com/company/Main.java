89c0ee6b8877c432b781621e2a342f7da3370764
Mikael69-codeSlayer <mikael.sardor@studerande.plushogskolan.se>
2021-01-07 11:44:45 +0100

    We created a webserver
    It's not done yet, but we have done one request response function.
    
    Co-Authored-By: Axel W <72077876+Gondarc@users.noreply.github.com>
    Co-Authored-By: Jonte111 <72066499+Jonte111@users.noreply.github.com>
    Co-Authored-By: nicolebradleey <72066187+nicolebradleey@users.noreply.github.com>

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,8 +6,24 @@ public class Main {
 
     public static void main(String[] args) {
 
+        // instantiate the app
         Express app = new Express();
-        app.listen(4000);
+
+        // "/" defaults to homepage
+        // req = Request;  res = Response
+        // req - server gets a question
+
+        // path is the endpoint url the method listens to
+        // res - answer to the question, for instance number, String or object information
+        app.get("/", (req, res) -> { // Lambda (arrow-function)
+            res.send("Hello World");
+
+            // timeline: 24:32
+        });
+
+        // listen starts the server
+        // and should be done after endpoint-listeners
+        app.listen(4000); // note that we can't use the same port as Vue does (3000)
 
     }
 }
