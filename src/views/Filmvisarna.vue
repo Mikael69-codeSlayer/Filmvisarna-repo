a664c66040ea692b513da2fe59366ae366b5b144
Jonte111 <jonathan.pe@telia.com>
2021-01-12 11:23:59 +0100

    Update Filmvisarna.vue
    Changed comment, tested style scoped, it works. Also tested to add message to "Filmvisarna" with the help of data return msg.

--- src/views/Filmvisarna.vue
+++ src/views/Filmvisarna.vue
@@ -1,19 +1,29 @@
 <!--   HTML  -->
 <template>
+<div class="home">
     <h1>Homepage</h1>
+    <h1>{{msg}}</h1>
+</div>
 </template>
 
 <!--  JavaScript (Vue.js)   -->
 <script>
 export default {
+    name: 'home',
+    data(){
+        return{
+            msg: 'Detta är ett test'
+        }
+    }
  
 };
 </script>
 
 <!--- CSS  --->
-<style>
+<style scoped>
+
 /*
-IMPORTANT! Don't forget to use 'scope'
+IMPORTANT! Don't forget to use 'scoped'
 otherwise all changes will effect other files
 */
 </style>
