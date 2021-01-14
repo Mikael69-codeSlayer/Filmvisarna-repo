c6c2f1296c92a15bd8d02d3579bf0da1e3960515
Mikael69-codeSlayer <mikael.sardor@studerande.plushogskolan.se>
2021-01-14 16:03:54 +0100

    Creating divs in filmerDetails
    Co-Authored-By: Kristian S <72066289+thedyze@users.noreply.github.com>
    Co-Authored-By: Jonte111 <72066499+Jonte111@users.noreply.github.com>

--- src/views/FilmerDetails.vue
+++ src/views/FilmerDetails.vue
@@ -1,16 +1,19 @@
 <template>
-  <h1>Filmer</h1>
-
+  
   <!-- It works. 
   We added v-for to loop through films like we did in 'Filmvisarna.vue'
   and v-if to choose which poster should be displayed. -->
-
+<div class="detail-container">
+  
   <div class="detail-posters" v-for="film of filmer" :key="film.id">
     <div v-if="film.id == 1">
       <img :src="film.posterUrl" />
+    </div>
+    <div class="detail-text">
       <p>{{ film.title }} | {{ film.length }}</p>
     </div>
   </div>
+</div>
 </template>
 
 <script>
@@ -29,7 +32,30 @@ export default {
 </script>
 
 <style scoped>
+
+div.detail-container {
+  background-color: blueviolet;
+  margin: 0 auto;
+}
+
 .detail-posters img {
   width: 200px;
+
 }
+
+div.detail-text {
+  background-color: bisque;
+  float: left;
+  width: 300px;
+}
+
+.detail-posters {
+  background-color: cadetblue;
+  width: 700px;
+  margin: 0 auto;
+  text-align: left;
+  
+}
+
+
 </style> 
