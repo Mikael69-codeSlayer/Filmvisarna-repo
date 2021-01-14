fe6db00acb6ba130843a72be3ec09ecad728146c
Mikael69-codeSlayer <mikael.sardor@studerande.plushogskolan.se>
2021-01-14 10:51:41 +0100

    Testing
    Co-Authored-By: Kristian S <72066289+thedyze@users.noreply.github.com>
    Co-Authored-By: Jonte111 <72066499+Jonte111@users.noreply.github.com>

--- src/views/FilmerDetails.vue
+++ src/views/FilmerDetails.vue
@@ -1,6 +1,25 @@
 <template>
   <h1>Filmer</h1>
   <h3>Param: {{ id }}</h3>
+   
+        
+      <!--    <div class="movie-details">
+            <div class="movie-details-poster">
+              <img :src="film.posterUrl" />
+            </div>
+
+            <div class="movie-item-text">
+              <p>{{ film.title }}</p> <br />
+              {{ film.genre }} 
+              | {{ film.length }} min
+              | {{ film.language }}
+            </div>
+          </div> -->
+ 
+ 
+      
+       
+
 </template>
 
 <script>
@@ -14,5 +33,5 @@ export default {
 };
 </script>
 
-<style scope>
+<style scoped>
 </style> 
