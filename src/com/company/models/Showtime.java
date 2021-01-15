ef71682924eac36d1f8ba9c96a53552bf526c262
Jonte111 <jonathan.pe@telia.com>
2021-01-15 07:18:22 +0100

    Made Showtime.java

---
+++ src/com/company/models/Showtime.java
@@ -0,0 +1,46 @@
+package com.company.models;
+
+import express.database.Model;
+import org.dizitart.no2.objects.Id;
+
+@Model
+public class Showtime {
+    @Id
+    private String auditorium;
+    private String film;
+    private String date;
+    private String time;
+    
+    public String getAuditorium() {
+        return auditorium;
+    }
+
+    public void setAuditorium(String auditorium) {
+        this.auditorium = auditorium;
+    }
+
+    public String getFilm() {
+        return film;
+    }
+
+    public void setFilm(String film) {
+        this.film = film;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public String getTime() {
+        return time;
+    }
+
+    public void setTime(String time) {
+        this.time = time;
+    }
+
+}
