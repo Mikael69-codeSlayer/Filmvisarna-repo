cf157b8db388b89e19604c18c1f63f34d5141fb4
Mikael69-codeSlayer <mikael.sardor@studerande.plushogskolan.se>
2021-01-15 10:41:03 +0100

    Added movie-information in filmerDetails
    Co-Authored-By: Jonte111 <72066499+Jonte111@users.noreply.github.com>
    Co-Authored-By: Kristian S <72066289+thedyze@users.noreply.github.com>

--- src/views/FilmerDetails.vue
+++ src/views/FilmerDetails.vue
@@ -3,24 +3,43 @@
   We added v-for to loop through films like we did in 'Filmvisarna.vue'
   and v-if to choose which poster should be displayed. -->
   <div class="detail-container" v-for="film of filmer" :key="film.id">
-      <div v-if="film.id == 1">
-       
-       <!-- <img :src="film.images[1]"> -->
-         
+    <div v-if="film.id == 1">
+      <!-- <img :src="film.images[1]"> -->
     </div>
-  <div class="movie-container">
-    <div class="detail-posters">
+    <div class="movie-container">
+      <div class="detail-posters">
         <img :src="film.posterUrl" />
         <div class="detail-text">
           <h1>{{ film.title }}</h1>
           <br />
-          <p>{{ film.genre }}
-          | {{ film.length }} min</p>
+          <p>{{ film.genre }} | {{ film.length }} min</p>
         </div>
       </div>
     </div>
+    
+    <div class="movie-info-container">
+      <div class="movie-description">
+        <p>{{ film.description }}</p>
+      </div>
+      
+      <div class="movie-director">
+        <p>Regi:<br />{{ film.director }}</p>
+      </div>
+
+      <div class="movie-actors">
+        <p>Skådespelare:<br />{{ film.actors }}</p>
+      </div>
+
+      <div class="movie-language">
+        <p>Originalspråk:<br />{{ film.language }}</p>
+      </div>
+    
+     <div class="movie-year">
+        <p>Utgivningsår:<br />{{ film.productionYear}}</p>
+      </div>
+
+    </div>
   </div>
-  
 </template>
 
 <script>
@@ -33,14 +52,12 @@ export default {
     id() {
       // get id from url parameter
       return this.$route.params.id;
-    }
-  }
+    },
+  },
 };
 </script>
 
 <style scoped>
-
-
 div.detail-text {
   background-color: rgba(93, 218, 93, 0);
   width: 400px;
@@ -50,26 +67,22 @@ div.detail-text {
 }
 
 div.detail-text h1 {
-    font-weight: bold;
-    font-size: 1.6em;
-    line-height: 2px;
-    color: white;
+  font-weight: bold;
+  font-size: 1.6em;
+  line-height: 2px;
+  color: white;
 }
 
 div.detail-text p {
-    line-height: 2px;
-    color: white;
-    font-size: 1em;
+  line-height: 2px;
+  color: white;
+  font-size: 1em;
 }
-
 
 div.detail-container {
-  background-color: rgb(141, 106, 42);
   height: 600px;
   width: 100%;
-  
 }
-
 
 /*div.inner-background img{
   background-color: orange;
@@ -81,7 +94,7 @@ div.detail-container {
 div.movie-container {
   background-color: blueviolet;
   padding-top: 400px;
-  background-image: url('https://variety.com/wp-content/uploads/2020/10/borat-subsequent-moviefilm-Borat_Subsequent_Moviefilm_00038_V2_rgb.jpg');
+  background-image: url("https://variety.com/wp-content/uploads/2020/10/borat-subsequent-moviefilm-Borat_Subsequent_Moviefilm_00038_V2_rgb.jpg");
   position: relative;
   background-repeat: no-repeat;
   background-size: cover;
@@ -91,12 +104,49 @@ div.movie-container {
   width: 200px;
 }
 
-
-
 .detail-posters {
   background-color: rgba(139, 225, 230, 0);
   width: 700px;
   margin: 0 auto;
   text-align: left;
 }
+
+div.movie-info-container {
+  height: 500px;
+  text-align: center;
+  font-family: "Roboto Slab", serif;
+  color: white;
+  padding-top: 30px;
+}
+
+div.movie-description {
+  width: 37%;
+  margin: 0 auto;
+  text-align: left;
+}
+
+div.movie-director {
+  width: 37%;
+  margin: 0 auto;
+  text-align: left;
+}
+
+div.movie-actors {
+  width: 37%;
+  margin: 0 auto;
+  text-align: left;
+}
+
+div.movie-language {
+  width: 37%;
+  margin: 0 auto;
+  text-align: left;
+}
+
+div.movie-year {
+  width: 37%;
+  margin: 0 auto;
+  text-align: left;
+}
+
 </style> 
