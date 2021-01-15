c576cbd49e891df886d420ad53cc3cfe69bd0537
Mikael69-codeSlayer <mikael.sardor@studerande.plushogskolan.se>
2021-01-15 13:48:33 +0100

    .
    Co-Authored-By: Jonte111 <72066499+Jonte111@users.noreply.github.com>
    Co-Authored-By: Kristian S <72066289+thedyze@users.noreply.github.com>

--- out/production/Filmvisarna/views/Login.vue
+++ out/production/Filmvisarna/views/Login.vue
@@ -1,26 +1,26 @@
 <template>
+
   <div id="login">
-    <h1 class="loginbody">Login</h1>
+    <h1 class="loginbody">Logga in</h1>
     <div class="inputboxes">
-      <input
-        type="text"
-        name="användarnamn"
-        v-model="input.anvandarnamn"
-        placeholder="Användarnamn"
-      />
-      <input
-        type="lösenord"
-        name="lösenord"
-        v-model="input.losenord"
-        placeholder="Lösenord"
-      />
+      <input type="text" name="username" v-model="input.username" placeholder="E-postadress"
+    />
+    <input type="password" name="password" v-model="input.password" placeholder="Lösenord"/>
     </div>
     <div class="buttonsbody">
-      <button type="button" @click="login()">Login</button>
-      <p>---eller---</p>
-      <button type="bliMedlem" @click="bliMedlem()">Skapa konto</button>
+    <button type="login" @click="login()">Logga in</button>
+
+    <p>---eller---</p>
+
+    <router-link :to="'/skapaKonto/'" >  
+    <button type="register">Skapa konto</button>
+    </router-link>
+  
+
     </div>
+ 
   </div>
+  
 </template>
 
 <script>
@@ -29,14 +29,15 @@ export default {
   data() {
     return {
       input: {
-        anvandarnamn: "",
-        losenord: "",
+        username: "",
+        password: "",
       },
     };
   },
   methods: {
-    login() {},
-    bliMedlem() {},
+    handlelogin(){
+     
+    }
   },
 };
 </script>
@@ -49,7 +50,6 @@ otherwise all changes will effect other files
 
 #login {
   width: 300px;
-  background-color: rgba(209, 6, 47, 0.973);
   margin: auto;
   margin-top: 100px;
   padding: 20px;
@@ -60,16 +60,24 @@ otherwise all changes will effect other files
   letter-spacing: 1px;
 }
 
-.inputboxes {
-  text-align: center;
-  border-radius: 20px;
-  box-sizing: border-box;
-  border: none;
-  font-weight: bold;
+.inputboxes input{
+  margin: 10px 0 5px;
+ text-align: center; 
+ border-radius:2px;
+ border:none;
+ font-family: "Roboto Slab", serif;
+  font-size: 15px;
 }
 
-.buttonsbody {
-  text-align: center;
-  margin-top: 10px;
+button {
+text-align: center;
+margin-top: 10px;
+border-radius: 2px;
+font-family: "Roboto Slab", serif;
+font-size: 15px;
+color: red;
+border: 0;
+padding:5px 10px;
+
 }
 </style>
---
+++ out/production/Filmvisarna/views/SkapaKonto.vue
@@ -0,0 +1,23 @@
+<template>
+  
+ <h1>Skapa konto</h1>
+
+</template>
+
+<script>
+
+export default {
+  computed: {
+    id() {
+      // get id from url parameter
+      return this.$route.params.id;
+    },
+  },
+};
+</script>
+
+<style scoped>
+h1{
+  color:white;
+}
+</style> 
