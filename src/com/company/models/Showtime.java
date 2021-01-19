937c61b60945e53fb2ccb2d473a51659f87acd8d
Jonte111 <jonathan.pe@telia.com>
2021-01-19 17:17:11 +0100

    Made Showtime and Salon classes
    Uploaded JSON and tested it.
    
    Co-Authored-By: Mikael <72066291+Mikael69-codeSlayer@users.noreply.github.com>

---
+++ src/com/company/models/Showtime.java
@@ -0,0 +1,63 @@
+package com.company.models;
+
+import express.database.Model;
+import org.dizitart.no2.objects.Id;
+
+@Model
+public class Showtime {
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getSalon() {
+        return salon;
+    }
+
+    public void setSalon(String salon) {
+        this.salon = salon;
+    }
+
+    public String getFilm() {
+        return film;
+    }
+
+    public void setFilm(String film) {
+        this.film = film;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public String getTime() {
+        return time;
+    }
+
+    public void setTime(String time) {
+        this.time = time;
+    }
+
+    public String getShowId() {
+        return showId;
+    }
+
+    public void setShowId(String showId) {
+        this.showId = showId;
+    }
+
+    @Id
+    private String id;
+    private String salon;
+    private String film;
+    private String date;
+    private String time;
+    private String showId;
+}
