2e9bcba2c9cacfc05fd4d9b6c6d090b3d4ebdaa7
Jonte111 <jonathan.pe@telia.com>
2021-01-29 08:59:08 +0100

    Update Tickets.vue
    Now shows available seats for this showing on Tickets page.
    
    Co-Authored-By: nicolebradleey <72066187+nicolebradleey@users.noreply.github.com>
    Co-Authored-By: Mikael <72066291+Mikael69-codeSlayer@users.noreply.github.com>
    Co-Authored-By: Kristian S <72066289+thedyze@users.noreply.github.com>

--- src/views/Tickets.vue
+++ src/views/Tickets.vue
@@ -1,10 +1,51 @@
 <template>
+<div>
+  <div class="available-seats">
+      <div v-if="seatsLeft" class="movie-list">
+        <p>Lediga säten {{ seatsLeft.availableSeats }}</p>
+      </div>
+  </div>
+
+
+
+
+
+
+
+
+</div>
   
 </template>
 
 <script>
 export default {
-
+computed: {
+    id() {
+      // get id from url parameter
+      return this.$route.params.id;
+    },
+    seatsLeft() {
+      // Also added this, to get filmes and id
+      return this.$store.state.showtime.filter(
+        (showtime) => showtime.id == this.id
+      )[0];
+      // this.id)[0]; alltid en stor vilket blir retunerad
+      //array[0]
+    },userLoggedIn() {
+      return this.$store.state.user == null
+  },
+   isLoggedIn(){
+      return this.$store.state.user != null
+    },
+    // images() {
+    //   return this.$store.state.images.filter((images) => images.id == this.id);
+    // },
+    showtime() {
+      return this.$store.state.showtime.filter(
+        (showtime) => showtime.film == this.film.title
+      );
+    },
+}
 }
 </script>
 
