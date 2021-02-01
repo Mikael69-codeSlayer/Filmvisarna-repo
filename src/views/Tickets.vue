ebb9ffcea2eb31638ab0da63bbc2cef526bb97a2
Mikael69-codeSlayer <mikael.sardor@studerande.plushogskolan.se>
2021-02-01 15:37:26 +0100

    Added and styled buttons in Tickets.vue
    Co-Authored-By: Jonte111 <72066499+Jonte111@users.noreply.github.com>

--- src/views/Tickets.vue
+++ src/views/Tickets.vue
@@ -6,18 +6,44 @@
       </div>
       <h1 id="choose-amount">Välj antal biljetter</h1>
       <div class="buttons-container">
+        <!-- Adult tickets -->
         <div class="adult-ticket-container">  
              <div class="adult-header">Vuxen 
                <p class="adult-price">120kr/st</p>
              </div>
+                <!-- plus and minus buttons -->
+             <div class="increment-buttons"> 
+              <button class="aButton" v-on:click.prevent="aIncrement">+</button>
+              <div class="aCount">{{ aCount }}</div>
+            <button class="aButton" v-on:click.prevent="aDecrement">-</button>
+            </div>
+            </div>
+              <!-- Child tickets -->
+              <div class="child-ticket-container">  
+             <div class="child-header">Barn 
+               <p class="child-price">80kr/st</p> 
+             </div>
+               <!-- plus and minus buttons -->
+             <div class="increment-buttons"> 
+              <button class="cButton" v-on:click.prevent="cIncrement">+</button>
+              <div class="cCount">{{ cCount }}</div>
+            <button class="cButton" v-on:click.prevent="cDecrement">-</button>
+            </div>
+            </div>
+                  <!-- Senior tickets -->
+                <div class="senior-ticket-container">  
+             <div class="senior-header">Pensionär 
+               <p class="senior-price">80kr/st</p> 
+             </div>
                
             
              <div class="increment-buttons"> 
-              <button class="button" v-on:click.prevent="increment">+</button>
-              <div class="count">{{ count }}</div>
-            <button class="button" v-on:click.prevent="decrement">-</button>
+              <button class="sButton" v-on:click.prevent="sIncrement">+</button>
+              <div class="sCount">{{ sCount }}</div>
+            <button class="sButton" v-on:click.prevent="sDecrement">-</button>
             </div>
             </div>
+            
             <button v-on:click="updateAvailableSeats, writeSeatsLeft, bookTicket" class="book-adult-ticket">Boka</button>
       </div>
     </div>
@@ -29,7 +55,9 @@ export default {
   name:"bookTicket",
   data() {
     return {
-      count: 0,
+      aCount : 0,
+      cCount: 0,
+      sCount: 0,
       availableSeats:'',
       userId:"",
       film: "",
@@ -52,7 +80,8 @@ export default {
       )[0];
     },
         updateAvailableSeats() {
-      this.seatsLeft.availableSeats = this.seatsLeft.availableSeats - this.count;
+      this.seatsLeft.availableSeats = this.seatsLeft.availableSeats - (this.aCount + this.cCount + this.sCount);
+       
       //console.log(this.seatsLeft.availableSeats);
       
     },
@@ -67,17 +96,43 @@ export default {
         this.$store.commit('setShowtime', list);
     },
 
-    increment() {
-      if(this.count < 8){
-        this.count++;
+    aIncrement() {
+      if(this. aCount  < 8){
+        this. aCount ++;
       }
     },
 
-    decrement() {
-      if (this.count > 0) {
-        this.count--;
+    aDecrement() {
+      if (this. aCount  > 0) {
+        this.aCount --;
       }
     },
+
+    cIncrement() {
+      if(this. cCount  < 8){
+        this. cCount ++;
+      }
+    },
+
+    cDecrement() {
+      if (this. cCount  > 0) {
+        this.cCount --;
+      }
+    },
+     sIncrement() {
+      if(this. sCount  < 8){
+        this. sCount ++;
+      }
+    },
+
+    sDecrement() {
+      if (this. sCount  > 0) {
+        this.sCount --;
+      }
+    },
+
+
+    
 
     bookTicket() {
       const ticket = {
@@ -108,11 +163,12 @@ export default {
   font-family: "Roboto Slab", serif;
 }
 
-div.adult-price{
-  
-  float: left;
-  padding-top: 30px;
-  padding-right: 50px;
+/*/--/--/--/--/--/--/--/--/--/--/--/--/--/--/--/- ADULT TICKET -/--/--/--/--/--/--/--/--/--/--/--/--/--/*/ 
+
+.adult-price{
+   color: black;
+  font-size: 15px;
+ 
 }
 
 div.available-seats {
@@ -129,12 +185,9 @@ div.adult-header {
    font-size: 20px;
    line-height: 15px;
 }
-.adult-price {
-  color: black;
-  font-size: 15px;
-}
 
-div.count {
+
+div.aCount {
   float: left;
   margin: 0 auto;
   text-align: center;
@@ -145,7 +198,7 @@ div.count {
     line-height: 30px;
 }
 
-.button {
+.aButton {
   float: left;
   margin: 0 auto;
   line-height: 30px;
@@ -173,4 +226,116 @@ div.adult-ticket-container{
   padding-bottom: 20px;
   font-weight: bold;
 }
+
+
+/*/--/--/--/--/--/--/--/--/--/--/--/--/--/--/--/- CHILD TICKET -/--/--/--/--/--/--/--/--/--/--/--/--/--/*/ 
+
+
+.child-price{
+   color: black;
+  font-size: 15px;
+
+ 
+}
+
+
+div.child-header {
+  float: left;
+  padding-right: 300px;
+  color: whitesmoke;
+   font-family: "Roboto Slab", serif;
+   font-size: 20px;
+   line-height: 15px;
+}
+
+
+div.cCount {
+  float: left;
+  margin: 0 auto;
+  text-align: center;
+  width: 40px;
+    color: whitesmoke;
+   font-family: "Roboto Slab", serif;
+    font-size: 20px;
+    line-height: 30px;
+}
+
+.cButton {
+  float: left;
+  margin: 0 auto;
+  line-height: 30px;
+  width: 30px;
+  border-radius: 100px;
+  border: none;
+  cursor: pointer;
+  outline: none;
+} 
+
+
+div.child-ticket-container{
+    padding: 30px;
+    font-weight: bold;
+    width: 500px;
+    text-align: left;
+    background-color: rgba(255, 228, 196, 0.342);
+     margin: 0 auto;
+     height: 60px;
+     
+  }
+
+/*/--/--/--/--/--/--/--/--/--/--/--/--/--/--/--/- SENIOR TICKET -/--/--/--/--/--/--/--/--/--/--/--/--/--/*/ 
+
+.senior-price{
+   color: black;
+  font-size: 15px;
+
+ 
+}
+
+
+div.senior-header {
+  float: left;
+  padding-right: 300px;
+  color: whitesmoke;
+   font-family: "Roboto Slab", serif;
+   font-size: 20px;
+   line-height: 15px;
+}
+
+
+div.sCount {
+  float: left;
+  margin: 0 auto;
+  text-align: center;
+  width: 40px;
+    color: whitesmoke;
+   font-family: "Roboto Slab", serif;
+    font-size: 20px;
+    line-height: 30px;
+}
+
+.sButton {
+  float: left;
+  margin: 0 auto;
+  line-height: 30px;
+  width: 30px;
+  border-radius: 100px;
+  border: none;
+  cursor: pointer;
+  outline: none;
+} 
+
+
+div.senior-ticket-container{
+    padding: 30px;
+    font-weight: bold;
+    width: 500px;
+    text-align: left;
+    background-color: rgba(255, 228, 196, 0.342);
+     margin: 0 auto;
+     height: 60px;
+  }
+
+
+
 </style>
